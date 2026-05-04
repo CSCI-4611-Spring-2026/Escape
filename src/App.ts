594180d1febdba7bfadac54cbab4eea1e97ae17a
Dan Orban <dtorban@umn.edu>
2026-05-04 07:39:44 -0500

    Update App.ts

--- src/App.ts
+++ src/App.ts
@@ -1,9 +1,4 @@
-/* Assignment 3: Earthquake Visualization
- * Concept and C++ implementation by Daniel Keefe and TAs, 2012+
- * GopherGfx implementation by Evan Suma Rosenberg <suma@umn.edu> 2022
- * License: Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International
- * PUBLIC DISTRIBUTION OF SOURCE CODE OUTSIDE OF CSCI 4611 IS PROHIBITED
- */
+
 
 import * as gfx from 'gophergfx'
 import { GUI } from 'dat.gui'
